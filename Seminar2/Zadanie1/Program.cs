c9eaebe9ccdbb7a591f8b8173d505f2191749005
Абрамович-Дроздова Аделина <001adelina@mail.ru>
2022-09-12 15:47:03 +0300

    realized Seminar2

---
+++ Seminar2/Zadanie1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 10. 
+
+// Напишите программу, которая принимает на вход трёхзначное число и на
+// выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+
+var num = 0;
+var str = "";
+
+while(true) {
+Console.WriteLine("Введите трёхзначное число: ");
+str = Console.ReadLine();
+if(int.TryParse(str, out int number)) {
+    num = number;
+    if(num < 0) { str = str.Remove(0,1); num = num * -1; }
+    if(str.Length == 3) {
+        break;
+    }
+    else Console.WriteLine("Введено НЕ трёхзначное число!");
+}
+else Console.WriteLine("Некорректно введено число!");
+}
+
+Console.WriteLine("Вторая цифра числа " + num + ": " + str[1]);
---
+++ Seminar2/Zadanie2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 13: 
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+Console.WriteLine("Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+var num = 0;
+var str = "";
+
+while(true) {
+Console.WriteLine("Введите любое число: ");
+str = Console.ReadLine();
+if(int.TryParse(str, out int number)) {
+    num = number;
+    if(num < 0) { str = str.Remove(0,1); num = num * -1; }
+    if(str.Length < 3) {
+        Console.WriteLine("Третьей цифры у числа " + num + " нет!");
+        break;
+    }
+    else {
+        Console.WriteLine("Третья цифра числа " + num + ": " + str[2]);
+        break;
+    }
+}
+else Console.WriteLine("Некорректно введено число!");
+}
